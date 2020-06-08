e67eb4ff704e87f7b3de2fa07038d4d9d35b1fad
Mitesh Modi <mitesh.modi003@gmail.com>
2020-06-08 19:58:08 -0400

    sequelized config and using env var

--- controllers/registeradmin.js
+++ controllers/registeradmin.js
@@ -2,9 +2,8 @@
 // rendering index.handlebars with title attributes, 
 // passing only necessary css files and js files dynamically
 exports.getDefaultPage = (req, res, next) => {
-    res.render('index' , {
-        title: 'PetMe | Home',
-        css: [ 'index.css' ],
-        GeoLocationApi: process.env.GEOLOCATION_API_KEY
+    res.render('resgisteradmin' , {
+        title: 'Register Admin',
+        css: [ 'index.css', 'registeradmin.css' ]
     });
 };
